2b25b9d2c8d570555e9b6fce9a3066cc7dd8300a
lishuang1103 <1667568351@qq.com>
2025-02-05 19:41:09 -0500

    add initial UI

---
+++ library-system/src/pages/BookManagement.jsx
@@ -0,0 +1,16 @@
+export default function BookManagement() {
+    return (
+      <div>
+        <h2>Book management</h2>
+        <div style={{ 
+          marginTop: 20,
+          padding: 24,
+          background: '#fff',
+          borderRadius: 8,
+          minHeight: 400
+        }}>
+          <p>The library management function is under development</p >
+        </div>
+      </div>
+    )
+  }
---
+++ library-system/src/pages/index.jsx
@@ -0,0 +1,4 @@
+export { default as Dashboard } from './Dashboard';
+export { default as BookManagement } from './BookManagement';
+export { default as UserCenter } from './UserCenter';
+export { default as Reports } from './Reports';
