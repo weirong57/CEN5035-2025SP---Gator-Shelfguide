d121350b5b66432706600f6254bf0521280e2851
weirong57 <weirong2023@163.com>
2025-04-18 16:27:31 -0400

    Initial commit: backend for library management system

--- library-backend/routes/borrowRoutes.go
+++ library-backend/routes/borrowRoutes.go
@@ -3,12 +3,14 @@ package routes
 import (
 	"library-backend/controllers"
 	"net/http"
-
+	"log" 
 	"github.com/gorilla/mux"
 )
 
 // BorrowRoutes 处理借书和还书的路由 / Handle book borrowing and returning routes
 func BorrowRoutes(router *mux.Router) {
+
+	log.Println("📌 Registering /api/borrow and /api/borrow/return routes") 
 	router.HandleFunc("/borrow", controllers.BorrowBook).Methods(http.MethodPost)
 	router.HandleFunc("/borrow/return", controllers.ReturnBook).Methods(http.MethodPost)
 }
